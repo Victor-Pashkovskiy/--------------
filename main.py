0b79e6a4654f22d33f50e79bcbda03cf5257b65c
Victor Pshkovskiy <banankalash@gmail.com>
2023-11-20 19:53:18 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import eel
+import pyowm
+@eel.expose
+def get_weather(place):
+    owm = pyowm.OWM('e059443947dca2151be09d4f4d0dee09') #Апи ключ с сайта https://home.openweathermap.org/api_keys
+    mgr = owm.weather_manager()
+    observation = mgr.weather_at_place(city)
+    w = observation.weather
+
+    temp=w.temperature('celsius')['temp']
+    return 'В городе' + city + 'сейчас' + str(temp) + 'градусов!'
+eel.init('web')
+
+eel.start('main.html', size=(700,700))
