8d9717629f588a90d981b24827030dd807669530
Victor Pshkovskiy <banankalash@gmail.com>
2023-11-24 20:08:37 +0300

    1.1

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import pyowm
 def get_weather(place):
     owm = pyowm.OWM('e059443947dca2151be09d4f4d0dee09') #Апи ключ с сайта https://home.openweathermap.org/api_keys
     mgr = owm.weather_manager()
-    observation = mgr.weather_at_place(city)
+    observation = mgr.weather_at_place(place)
     w = observation.weather
 
     temp=w.temperature('celsius')['temp']
-    return 'В городе' + city + 'сейчас' + str(temp) + 'градусов!'
+    return 'В городе' + place + 'сейчас' + str(temp) + 'градусов!'
 eel.init('web')
 
 eel.start('main.html', size=(700,700))
